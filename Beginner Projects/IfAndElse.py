b7287d8cf35412207b8ced571d6dcc76a39472fe
vertalQ <vertaQ0@gmail.com>
2022-12-31 21:25:16 -0700

    Added 'IfAndElse.py' file.

---
+++ Beginner Projects/IfAndElse.py
@@ -0,0 +1,15 @@
+age = 19 
+##-------------------------------
+
+#if age != 20:
+#   print('You are too old!')
+#   print('Why are you here?')
+#   print('Why aren\'t you mowing a lawn or sorting papers?')
+
+
+hair_color = 'brown'
+
+if hair_color == 'brown' or hair_color == 'black':
+    print('Your hair color is %s' % hair_color)
+elif hair_color != 'brown' or 'black':
+    print('Unregistered hair color: %s' % hair_color)
