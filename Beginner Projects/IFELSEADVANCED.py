d27c04a25615ebc0aafc6e51e4105b302356b5ea
vertalQ <vertaQ0@gmail.com>
2023-01-01 00:45:04 -0700

    Wrote new code in 'IFELSEADVANCED.py' file

--- Beginner Projects/IFELSEADVANCED.py
+++ Beginner Projects/IFELSEADVANCED.py
@@ -1,3 +1,12 @@
-myval = None
-print(myval)
+#myval = None
+#
+#if myval == None:
+#    print('There is not val!')
+#else:
+#    print("The val is %s" % myval)
 
+age = '10'
+
+if age == 10:
+    print("What's the best way to speak to a monster?")
+    print("From as far away as possible!")
