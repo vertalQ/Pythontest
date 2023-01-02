dde443c3922d3d65d45d98ac3b42c88343fddff8
vertalQ <vertaQ0@gmail.com>
2023-01-01 18:58:28 -0700

    Added new file 'ifstatementtestIDLE.py' and changed code in 'IFELSEADVANCED.py' file

--- Beginner Projects/IFELSEADVANCED.py
+++ Beginner Projects/IFELSEADVANCED.py
@@ -5,8 +5,9 @@
 #else:
 #    print("The val is %s" % myval)
 
-age = '10'
+age = 10.5
+converted_age = int(age)
 
-if age == 10:
+if converted_age == 10.5:
     print("What's the best way to speak to a monster?")
     print("From as far away as possible!")
