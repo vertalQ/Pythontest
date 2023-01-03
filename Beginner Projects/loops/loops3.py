d43032a4541a5ef9e2ae9faa8a4c6929b47fc58e
vertalQ <vertaQ0@gmail.com>
2023-01-02 17:17:56 -0700

    Added new line in 'loops3.py' file.

--- Beginner Projects/loops/loops3.py
+++ Beginner Projects/loops/loops3.py
@@ -2,6 +2,7 @@ found_coins = 20
 magic_coins = 70
 stolen_coins = 3
 coins = found_coins
+#---------------------------
 
 for week in range(1, 53):
     coins = coins + magic_coins - stolen_coins
@@ -12,3 +13,4 @@ if found_coins == 20:
     print("Untouched and safe.")
 else:
     print("Value has been modified.")
+    
