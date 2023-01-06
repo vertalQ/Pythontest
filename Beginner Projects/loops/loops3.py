af7c3162d94d914425610d5958a6051b0d57112d
vertalQ <vertaQ0@gmail.com>
2023-01-05 21:19:06 -0700

    Added new code in 'loops3.py'.

--- Beginner Projects/loops/loops3.py
+++ Beginner Projects/loops/loops3.py
@@ -1,8 +1,13 @@
+import time
+
+#--------------------------
 found_coins = 20
 magic_coins = 70
 stolen_coins = 3
 coins = found_coins
 silly_list = ['one', 'apple', 'orange', 'peel']
+count = 0
+countGoal = 25
 #---------------------------
 
 #for week in range(1, 53):
@@ -15,5 +20,10 @@ silly_list = ['one', 'apple', 'orange', 'peel']
 #else:
 #   print("Value has been modified.")
 
-for i in silly_list:
-    print('%s is the %s item on the list' % (i, silly_list.index(i)))
+#for i in silly_list:
+#    print('%s is the %s item on the list' % (i, silly_list.index(i)))
+
+while count != countGoal:
+    time.sleep(0.5)
+    print(count)
+    count += 1
