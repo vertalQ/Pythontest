61a232803d81803b88edd083c03dd3b40d5f676b
vertalQ <vertaQ0@gmail.com>
2023-01-05 21:13:26 -0700

    Made changes to 'loops3.py'.

--- Beginner Projects/loops/loops3.py
+++ Beginner Projects/loops/loops3.py
@@ -2,15 +2,18 @@ found_coins = 20
 magic_coins = 70
 stolen_coins = 3
 coins = found_coins
+silly_list = ['one', 'apple', 'orange', 'peel']
 #---------------------------
 
-for week in range(1, 53):
-    coins = coins + magic_coins - stolen_coins
-    print('Week %s = %s coins' % (week, coins))
-print(found_coins)
+#for week in range(1, 53):
+#    coins = coins + magic_coins - stolen_coins
+#    print('Week %s = %s coins' % (week, coins))
+#print(found_coins)
+#
+#if found_coins == 20:
+#    print("Untouched and safe.")
+#else:
+#   print("Value has been modified.")
 
-if found_coins == 20:
-    print("Untouched and safe.")
-else:
-    print("Value has been modified.")
-    
+for i in silly_list:
+    print('%s is the %s item on the list' % (i, silly_list.index(i)))
