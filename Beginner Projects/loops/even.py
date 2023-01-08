153917f570420022c30e2cee56392b2ed16d05dd
vertalQ <vertaQ0@gmail.com>
2023-01-07 22:07:01 -0700

    Added the following files.
    * 'even.py'
    * 'fivefavoriteingredients.py'
    * 'helloloop.py'
    * 'moonweight.py'

---
+++ Beginner Projects/loops/even.py
@@ -0,0 +1,7 @@
+age = 22
+count = 0
+#===============
+
+while count != age:
+    count += 2
+    print(count)
---
+++ Beginner Projects/loops/fivefavoriteingredients.py
@@ -0,0 +1,6 @@
+ingredients = ['lettuce', 'turkey', 'bread', 'tomato', 'ketchup']
+indexval = 1
+
+for i in ingredients:
+    print(indexval, i)
+    indexval += 1
