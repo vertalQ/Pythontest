56b4ef201d2a311853918db90499780a0ee09ff4
vertalQ <vertaQ0@gmail.com>
2023-01-10 14:53:12 -0700

    Fixed 'tpitchfork.py' file code.

--- Beginner Projects/classes_objects/tpitchfork.py
+++ Beginner Projects/classes_objects/tpitchfork.py
@@ -3,9 +3,10 @@ import turtle
 class Lines:
     def __init__(self):
         self.t = turtle.Pen()
-        self.base = 50
-        self.side1 = 20
-        self.side2 = 30
+        self.base = 90
+        self.sbase = 80
+        self.side1 = 30
+        self.side2 = 60
     def line1div1(self):
         self.t.forward(self.base)
         self.t.left(90)
@@ -18,10 +19,27 @@ class Lines:
         self.t.forward(self.side1)
         self.t.left(90)
         self.t.forward(self.side1)
+    def line2div3(self):
+        self.t.forward(self.sbase)
+        self.t.right(90)
+        self.t.forward(self.side2)
+        self.t.left(90)
+        self.t.forward(self.side2)
+    def line2div4(self):
+        self.t.forward(self.sbase)
+        self.t.left(90)
+        self.t.forward(self.side2)
+        self.t.right(90)
+        self.t.forward(self.side2)
 
 r = Lines()
-r.line1div1
-r.line1div2
+y = Lines()
+ry = Lines()
+yr = Lines()
+r.line1div1()
+y.line1div2()
+ry.line2div3()
+yr.line2div4()
 
 #---------------------------
 turtle.mainloop()
