9a56dd9ac10b97eff5825b4281201de56af3e968
vertalQ <vertaQ0@gmail.com>
2023-01-09 23:39:37 -0700

    Made changes to 'classes1.py'.

--- Beginner Projects/classes_objects/classes1.py
+++ Beginner Projects/classes_objects/classes1.py
@@ -32,7 +32,21 @@ class Giraffes(Mammals):
     def __init__(self, spots, friend):
         self.giraffe_spots = spots
         self.friend = friend
-
+    def left_foot_forward(self):
+        print('left foot forward')
+    def left_foot_backward(self):
+        print('left food backward')
+    def right_foot_forward(self):
+        print('right foot forward')
+    def right_foot_backward(self):
+        print('right food backward')
+    def dance(self):
+        self.left_foot_backward()
+        self.left_foot_forward()
+        self.right_foot_backward()
+        self.right_foot_forward()
+        self.left_foot_backward()
+        self.right_foot_forward()
     def find_food(self):
         self.move()
         print("I've found food!")
@@ -40,6 +54,9 @@ class Giraffes(Mammals):
     def sing(self):
         print('La la la!')
 
+
+dancingGiraffe = Giraffes(100, 'Unknown')
+dancingGiraffe.dance()
 
 #class count:
 #    def add(self):
@@ -50,9 +67,9 @@ class Giraffes(Mammals):
 
 #reginald = Giraffes()
 #harold = Giraffes()
-ozwald = Giraffes(100, 'Gertrude')
-gertude = Giraffes(1020, 'Ozwald')
+#ozwald = Giraffes(100, 'Gertrude')
+#gertude = Giraffes(1020, 'Ozwald')
+#print(ozwald.giraffe_spots, ozwald.friend)
+#print('...')
+#print(gertude.giraffe_spots, gertude.friend)
 
-print(ozwald.giraffe_spots, ozwald.friend)
-print('...')
-print(gertude.giraffe_spots, gertude.friend)
