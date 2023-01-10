ac29fa14c8cbaef17e8049cec8f38dd58f5c38ed
vertalQ <vertaQ0@gmail.com>
2023-01-09 22:38:45 -0700

    Added new code to 'classes1.py' file.

--- Beginner Projects/classes_objects/classes1.py
+++ Beginner Projects/classes_objects/classes1.py
@@ -1,3 +1,25 @@
 class Things:
     pass
+#--------------------------------
 
+class Inanimate(Things):
+    pass
+
+class Sidewalks(Inanimate):
+    pass
+
+#--------------------------------
+
+class Animate(Things):
+    pass
+
+class Animals(Animate):
+    pass
+
+class Mammals(Animals):
+    pass
+
+class Giraffes(Mammals):
+    pass
+
+#--------------------------------
