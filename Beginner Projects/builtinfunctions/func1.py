1c2344719a016f62f24c13f715a6da19e288be36
vertalQ <vertaQ0@gmail.com>
2023-01-10 15:19:32 -0700

    Added new directory and two new files.
    * Added new directory caled 'builtinfunctions' inside of the 'Beginner Projects' folder.
    * Added 'func1.py' file inside of 'builtinfunctions'
    * Added 'func2.py' file inside of 'buildinfunctions'

---
+++ Beginner Projects/builtinfunctions/func1.py
@@ -0,0 +1,7 @@
+#print(abs(10))
+#print(abs(-244))
+
+steps = -3
+
+if abs(steps) > 0:
+    print('Character is moving.')
---
+++ Beginner Projects/builtinfunctions/func2.py
@@ -0,0 +1,47 @@
+#print(bool(0))
+#print(bool(1))
+#print(bool(1123.23))
+#print(bool(-500))
+
+#----------------------------------------
+
+#print(bool(None))
+#print(bool('asidad'))
+#print(bool(' '))
+#print(bool('What do you call a pig doing karate? Pork Chop!'))
+
+#----------------------------------------
+
+#list = []
+#print(bool(list))
+#list = ['l', 'i', 's', 't']
+#print(bool(list))
+
+#----------------------------------------
+
+#year = input('Year of birth: ')
+
+#if not bool(year.rstrip()):
+#    print('You need to enter a value for your year of birth.')
+
+#----------------------------------------
+
+#print(dir(['a', 'short', 'list']))
+
+#----------------------------------------
+
+#popcorn = 'I love popcorn!'
+#print(dir(popcorn))
+#help(popcorn.upper)
+#print(popcorn.upper())
+
+#----------------------------------------
+
+#print(eval('10*5'))
+#print(eval('''if True:
+#                print('this won't work at all!'''))
+
+#----------------------------------------
+
+calc = input('Enter a calculation: ')
+print(eval(calc))
