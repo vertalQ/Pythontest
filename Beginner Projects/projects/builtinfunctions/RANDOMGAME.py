0c4589c745d0050b7426466656ed8ae2f95b24f9
vertalQ <vertaQ0@gmail.com>
2023-01-11 22:48:00 -0700

    Added more content to 'RANDOMGAME.py' game.

--- Beginner Projects/projects/builtinfunctions/RANDOMGAME.py
+++ Beginner Projects/projects/builtinfunctions/RANDOMGAME.py
@@ -1,6 +1,5 @@
-
+import random
 #------------------------------------------------------
-
 def gameChoice():
     while True:
         choice = int(input('''Pick a game by inputting the number: 
@@ -10,7 +9,8 @@ def gameChoice():
     4. Quit program.
     Game: '''))
         if choice == 1:
-            print("You are now playing: 'Number Guess.' ")
+            numberGuess()
+            break 
         elif choice == 2:
             print("You are now playing: 'Fruit Guess.'")
         elif choice == 3:
@@ -20,6 +20,25 @@ def gameChoice():
             break
         else:
             print('Invalid input. Please pick a number from the list.')
+
+def numberGuess():
+    print("You are now playing: 'Number Guess.' ")
+    while True: 
+        gusnum = random.randint(0,20)
+        playerGuess = int(input('Correctly guess a number from 0 to 20!: '))
+        if int(playerGuess) == gusnum:
+            chN = input('Well done! Would you like to play again? Y/N: ').lower()
+            if chN == 'n':
+                break
+            elif chN == 'menu':
+                gameChoice()
+                break
+        else:
+            print('Incorrect! Try again!')
+        
+
+        
+
 
 gameChoice()
     
