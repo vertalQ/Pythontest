fefe1615d7733a2193fe0f50103f974bfe60bdd0
vertalQ <vertaQ0@gmail.com>
2023-01-11 20:59:34 -0700

    Added 'lenfunc.py' and 'advancedlistread.py' files.

--- Beginner Projects/builtinfunctions/lenfunc.py
+++ Beginner Projects/builtinfunctions/lenfunc.py
@@ -1,10 +1,12 @@
-def name():
-    while True:
-        nameInp = input('Enter your name. Cannot be more than 12 characters!: ')
-        if len(nameInp) > 12:
-            print('Name is greater than 12 characters! Try again.')
-        else:
-            print('Name allowed!')
-            break
-
-name()
+#def name():
+#    while True:
+#        nameInp = input('Enter your name. Cannot be more than 12 characters!: ')
+#        if len(nameInp) > 12:
+#            print('Name is greater than 12 characters! Try again.')
+#        else:
+#            print('Name allowed!')
+#            break
+#name()
+#--------------------------------------------------------------------------
+creature_list = ['unicorn', 'cyclops', 'fairy', 'elf', 'dragon', 'troll']
+print('You have %s item(s) in this list!' % (len(creature_list)))
