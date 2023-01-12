b1a622cd73d6b002fd8848a7ba3daadf08394f59
vertalQ <vertaQ0@gmail.com>
2023-01-11 21:26:38 -0700

    Added 'mapRead.py' file.

--- Beginner Projects/projects/builtinfunctions/mapRead.py
+++ Beginner Projects/projects/builtinfunctions/mapRead.py
@@ -4,7 +4,21 @@ favFruitmap = {'Jason' : 'Apples',
 fruit = ['apple', 'banana', 'clementime', 'dragon fruit']
 length = len(fruit)
 #---------------------------------------------
-for i in range(length):
-    print('The fruit in index %s is %s' % (i, fruit[i]))
-    
-print(len(favFruitmap))
+#for i in range(length):
+    #print('The fruit in index %s is %s' % (i, fruit[i])) 
+#print(len(favFruitmap))
+#---------------------------------------------
+numbers = [5, 4, 10, 30, 22]
+#print(max(numbers))
+letters = 's,t,r,i,n,g,S,T,R,I,N,G'
+#print(max(letters))
+
+print(min(numbers))
+print(min(letters))
+#---------------------------------------------
+guess_this_number = 61
+player_guesses = [12, 15, 70, 45]
+if max (player_guesses) > guess_this_number:
+    print('Boom! You all lose.')
+else:
+    print('You win.')
