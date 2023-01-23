3739fb23c633b87475dae5e8fee1644201d8a117
vertalQ <vertaQ0@gmail.com>
2023-01-22 18:16:23 -0700

    Updated packages and added new file. - Added new file 'recap.py' in 'misc' dir. - Updated old libraries.

---
+++ Beginner Projects/misc/recap.py
@@ -0,0 +1,12 @@
+# Following code recovers content learned for Python so far.
+
+#Calculations and Variables.
+calcVars = [20, 1]
+funNum = calcVars[0] + calcVars[1]
+
+print(8 * 8)
+print(int(8 / 8))
+print(8 + 8)
+print(8 - 8)
+print("What's nine plus ten? %s. You stupid." % funNum)
+
