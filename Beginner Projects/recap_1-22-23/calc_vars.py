3e5565e873e7069a9dbebf8169c9ac260becf160
vertalQ <vertaQ0@gmail.com>
2023-01-22 18:32:48 -0700

    Added new file. - Added 'str_list_tup_maps.py' file inside 'recap_1-22-23' dir.

--- Beginner Projects/recap_1-22-23/calc_vars.py
+++ Beginner Projects/recap_1-22-23/calc_vars.py
@@ -1,9 +1,11 @@
-#Calculations and Variables.
+# Calculations and Variables.
 calcVars = [20, 1]
 funNum = calcVars[0] + calcVars[1]
+numStored = 93210
 
 print(8 * 8)
 print(int(8 / 8))
 print(8 + 8)
 print(8 - 8)
 print("What's nine plus ten? %s. You stupid." % funNum)
+print(numStored)
---
+++ Beginner Projects/recap_1-22-23/str_list_tup_maps.py
@@ -0,0 +1,26 @@
+#For printing clearly.
+blank = '    '
+# Strings, lists, tuples, and maps.
+
+string = 'This is a string.'
+list = ['This', 'is', 'a', 'list.']
+tuple = ('This', 'is', 'a', 'tuple.')
+map = {'1' : 'This',
+'2' : 'is',
+'3' : 'a',
+'4' : 'map.'}
+
+# The following uses for loops in order to print a the last 3 var types.
+
+for i in range(len(list)):
+    print(list[i])
+
+print(blank * 20)
+
+for i in range(len(tuple)):
+    print(tuple[i])
+
+print(blank * 20)
+
+for i in range(len(map)):
+    print(map[str(i+1)])   
