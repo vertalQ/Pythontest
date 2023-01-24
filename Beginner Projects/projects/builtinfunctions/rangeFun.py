b2792b97a8a68384efa2760a02e5e16513b29fd5
vertalQ <vertaQ0@gmail.com>
2023-01-23 17:48:34 -0700

    Added new files and modified older files.

---
+++ Beginner Projects/projects/builtinfunctions/rangeFun.py
@@ -0,0 +1,21 @@
+#for x in range (0, 5):
+#    print(x)
+#print(list(range(0, 5)))
+
+count_by_twos = list(range(0, 30, 2))
+count_down_by_twos = list(range(50, 0, -2))
+#---------------------------
+#print(count_by_twos)
+#print(count_down_by_twos)
+#for x in range(0, 50, 2):
+#    print(x)
+#    if x == 50:
+#        print('Reached 50')
+
+#---------------------------
+#listNums = list(range(0, 400, 20))
+#print(listNums)
+#print(sum(listNums))
+#---------------------------
+
+
