ba026ce455e79d51fee3f1723d27749888530dbb
vertalQ <vertaQ0@gmail.com>
2023-01-27 13:43:56 -0700

    Made final changes to 'day_night.py'.

--- Beginner Projects/misc/day_night.py
+++ Beginner Projects/misc/day_night.py
@@ -11,20 +11,30 @@ clockStart = DNTime()
 
 def clockwork():
     global clockStart
+    count_7 = 0
+    AMVAL = 'AM'
     while True:
         t.sleep(0.5)
-        print(clockStart.clock)
-        print(clockStart.AM)
         clockStart.clock += 1
+        count_7 += 1
+        if clockStart.AM == True:
+            AMVAL = 'AM'
+        else:
+            AMVAL = 'PM'
         if clockStart.clock == 12:
-            t.sleep(0.5)
-            print(clockStart.clock)
-            print(clockStart.AM)
             clockStart.clock = 1
             if clockStart.AM == True:
                 clockStart.AM = False
             elif clockStart.AM == False:
                 clockStart.AM = True
+        elif count_7 == 7:
+            count_7 = 0
+            timeReq = input('Would you lie to know the time? Y/N: ').lower()
+            if timeReq == 'y':
+                print('It is %s:00 %s!' % (clockStart.clock, AMVAL))
+                
+        
+        
 
 clockwork()
 
