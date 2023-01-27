b2db82b43b4dcc35d1e71d6f899028728eae7e60
vertalQ <vertaQ0@gmail.com>
2023-01-27 14:31:32 -0700

    Fixed 'itemindex.py' current ver.

--- Beginner Projects/misc/itemIndex.py
+++ Beginner Projects/misc/itemIndex.py
@@ -1,20 +1,21 @@
 itemDict = {
-'000' : 'air',
-'001' : 'apple',
-'002' : 'pear',
-'003' : 'low level sword',
-'004' : 'dragon flesh',
-'005' : 'cooked dragon meat'
+'0' : 'air',
+'1' : 'apple',
+'2' : 'pear',
+'3' : 'low level sword',
+'4' : 'dragon flesh',
+'5' : 'cooked dragon meat'
 }
 
 class Item:
     def __init__ (self):
-        self.index = 000
+        self.index = 0
 
 i = Item()
 
 def createItem():
     global itemDict
+    global i
     while int(i.index) <= int(max(itemDict.keys())):
         print('You are current on item %s' % (itemDict[str(i.index)]))
         i.index += 1
