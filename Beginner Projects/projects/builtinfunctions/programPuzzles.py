e35720156c476aa2c5432e3c261c1875e6cce12e
vertalQ <vertaQ0@gmail.com>
2023-01-28 00:45:40 -0700

    Modified code in 'programPuzzles.py' Note: Code is still unfunctional and additional work.

--- Beginner Projects/projects/builtinfunctions/programPuzzles.py
+++ Beginner Projects/projects/builtinfunctions/programPuzzles.py
@@ -4,12 +4,20 @@
 #print(b)
 #--------------------------------------
 
-funcStrDIR = list(dir('string'))
-totalNUM = len(funcStrDIR)
+#funcStrDIR = list(dir('string'))
+#totalNUM = len(funcStrDIR)
 
-def allFuncs():
-    global funcStrDIR
-    for i in range(0, totalNUM):
-        exec('''print(help('string'.%s))''' % funcStrDIR[i])
+#def allFuncs():
+#    global funcStrDIR
+#    for i in range(0, totalNUM):
+#        exec('''print(help('string'.%s))''' % funcStrDIR[i])
 #print(help('string'.split))
-allFuncs()
+#allFuncs()
+
+#--------------------------------------
+
+hiddenMessage = "this if is not are a reading very this good then way you to have hide done a it message wrong"
+HMLIST = hiddenMessage.splitlines()
+print(help('string'.splitlines))
+
+#print(HMLIST)
