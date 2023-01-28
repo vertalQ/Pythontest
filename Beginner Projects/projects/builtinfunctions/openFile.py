ff2b6e3d9a680b05ff92e554f2031271c2ea3978
vertalQ <vertaQ0@gmail.com>
2023-01-28 00:16:53 -0700

    Added new files. - Created 'testWrite.txt' file in 'misc' directory. - Added write and read functionality to 'openFile.py' for 'testWrite.txt'

--- Beginner Projects/projects/builtinfunctions/openFile.py
+++ Beginner Projects/projects/builtinfunctions/openFile.py
@@ -1,3 +1,14 @@
-open_file = open('C:\\Users\\abdul\\Desktop\\Git\\Pythontest\\Beginner Projects\\misc\\saveData.txt')
-readSaveData = open_file.read()
-print(readSaveData)
+#open_file = open('C:\\Users\\abdul\\Desktop\\Git\\Pythontest\\Beginner Projects\\misc\\saveData.txt')
+#readSaveData = open_file.read()
+#print(readSaveData)
+
+#--------------------------------------------------------------
+
+write_file = open('C:\\Users\\abdul\\Desktop\\Git\\Pythontest\\Beginner Projects\\misc\\testWrite.txt', 'w')
+write_file.write('''This is a test for writing a file in 'misc' directory''')
+write_file.close()
+
+#--------------------------------------------------------------
+
+read_file = open('C:\\Users\\abdul\\Desktop\\Git\\Pythontest\\Beginner Projects\\misc\\testWrite.txt')
+print(read_file.read())
