d5dfa0dcd2be0b47efda84698621e5f9eef6065e
vertalQ <vertaQ0@gmail.com>
2023-01-28 00:01:29 -0700

    Modified code in 'rangeFun.py'

--- Beginner Projects/projects/builtinfunctions/rangeFun.py
+++ Beginner Projects/projects/builtinfunctions/rangeFun.py
@@ -4,6 +4,8 @@
 
 count_by_twos = list(range(0, 30, 2))
 count_down_by_twos = list(range(50, 0, -2))
+stringList = ['Ohio', 'Apple']
+intList = [1, 2, 6]
 #---------------------------
 #print(count_by_twos)
 #print(count_down_by_twos)
@@ -11,11 +13,14 @@ count_down_by_twos = list(range(50, 0, -2))
 #    print(x)
 #    if x == 50:
 #        print('Reached 50')
-
 #---------------------------
 #listNums = list(range(0, 400, 20))
 #print(listNums)
 #print(sum(listNums))
 #---------------------------
+#for x in range(0, 4):
+#    print(x)
 
-
+print(list(range(0, 10, 2)))
+#print(sum(stringList))
+print(sum(intList))
